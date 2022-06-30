9789b2960d2bf61e382f9f92134b2d5c29ebf60f
kwchang0831 <88667293+kwchang0831@users.noreply.github.com>
2022-06-30 09:27:17 +0800

    :construction: wip: search btn

--- src/lib/header.svelte
+++ src/lib/header.svelte
@@ -5,24 +5,46 @@
   import { site } from '$config/site';
   import { theme } from '$lib/stores/themes';
   import { fly } from 'svelte/transition';
+  let search: boolean = false;
 </script>
 
 <header id="header" class="fixed w-screen all:(transition-500) ease-in-out border-transparent max-h-16 z-40">
-  <div
-    id="header-nav"
-    class="flex items-center justify-between border-transparent backdrop-blur"
-    in:fly={{ x: -50, duration: 300, delay: 300 }}
-    out:fly={{ x: -50, duration: 300 }}>
-    <a href="/" sveltekit:prefetch class="text-xl font-semibold normal-case mx-4 my-2 btn btn-ghost">{site.title}</a>
+  {#if !search}
+    <div
+      id="header-nav"
+      class="flex items-center border-transparent backdrop-blur py-2"
+      in:fly={{ x: -50, duration: 300, delay: 300 }}
+      out:fly={{ x: -50, duration: 300 }}>
+      <a href="/" sveltekit:prefetch class="text-xl font-semibold normal-case mx-4 btn btn-ghost">{site.title}</a>
 
-    {#key $theme}
-      <div class="all:transition-300">
-        <button aria-label="Dark Mode Switch" on:click={theme.toggle} class="mx-4 my-2 btn btn-ghost">
-          <div class="!w-[2rem] !h-[2rem] text-inherit i-carbon-sun dark:i-carbon-moon" />
+      <div class="ml-auto">
+        <button aria-label="search" on:click={() => (search = !search)} tabindex="0" class="btn btn-ghost">
+          <div class="!w-[2rem] !h-[2rem] i-carbon-search" />
         </button>
       </div>
-    {/key}
-  </div>
+      {#key $theme}
+        <div class="mr-4">
+          <button aria-label="Dark Mode Switch" on:click={theme.toggle} class="btn btn-ghost">
+            <div class="!w-[2rem] !h-[2rem] i-carbon-sun dark:i-carbon-moon" />
+          </button>
+        </div>
+      {/key}
+    </div>
+  {:else}
+    <div id="header-nav" class="flex items-center py-2" in:fly={{ x: 50, duration: 300, delay: 300 }} out:fly={{ x: 50, duration: 300 }}>
+      <form accept-charset="UTF-8" class="grow flex">
+        <input type="text" name="index-search" id="index-search" class="grow my-2 mx-4 rounded  bg-transparent border-1">
+        <button class="btn btn-ghost">
+          <div class="!w-[2rem] !h-[2rem] i-carbon-search" />
+        </button>
+      </form>
+      <div class="mr-4">
+        <button on:click={() => (search = !search)} class="btn btn-ghost">
+          <div class="!w-[2rem] !h-[2rem] i-carbon-close" />
+        </button>
+      </div>
+    </div>
+  {/if}
 </header>
 
 <style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,11 +4,11 @@
   import Footer from '$lib/footer.svelte';
 </script>
 
-<!--  h-screen justify-center lg:(flex-row flex-wrap) -->
 <div class="pt-[4rem] relative flex flex-col xl:(flex-row gap-8) justify-center">
   <div class="">
     <IndexProfile class="sticky top-20 flex flex-col items-center py-4 xl:(items-right text-right)" />
   </div>
   <IndexPosts class="" />
 </div>
+
 <Footer />
