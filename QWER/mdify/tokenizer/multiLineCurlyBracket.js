748b1013296dcff6436eaa62b68b90014651744b
kwchang0831 <88667293+kwchang0831@users.noreply.github.com>
2022-08-29 02:24:53 +0800

    :bug: fix: use match group instead raw

--- QWER/mdify/tokenizer/multiLineCurlyBracket.js
+++ QWER/mdify/tokenizer/multiLineCurlyBracket.js
@@ -2,18 +2,19 @@ export const multiLineCurlyBracket = {
   name: 'multiLineCurlyBracket',
   level: 'block',
   tokenizer(src) {
-    const rule = /^\{[\s\S]*(?<![\r\n{]{3,}[\s\S]*)}/;
+    const rule = /^\{([\s\S]*(?<![\r\n{]{3,}[\s\S]*))\}/;
     const match = rule.exec(src);
 
     if (match) {
       const token = {
         type: 'multiLineCurlyBracket',
         raw: match[0],
+        content: match[1].trim(),
       };
       return token;
     }
   },
   renderer(token) {
-    return `<p>{@html ${token.raw.slice(1, -1)}}</p>\n`;
+    return `<p>{@html ${token.content}}</p>\n`;
   },
 };
