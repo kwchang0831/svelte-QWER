ac9ea7fa772c121ebaef3f48cd237b50012eec0a
kwchang0831 <88667293+kwchang0831@users.noreply.github.com>
2022-09-11 02:50:32 +0800

    :bug: fix: error page 404 img

---
+++ .changeset/plenty-starfishes-shout.md
@@ -0,0 +1,5 @@
+---
+'@kwchang0831/svelte-qwer': patch
+---
+
+fix: error page only shows 404 img if status is actually 404
--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -16,7 +16,12 @@
     class="pt-[4rem] relative flex flex-col items-center gap-4 justify-center">
     <h1 class="text-6xl">{$page.status ?? '404'}</h1>
     <p class="text-lg">{$page.status === 404 ? $LL.Page404NotFound() : $page.error?.message}</p>
-    <img draggable="false" class="select-none max-w-lg w-full h-auto" src={Error404} alt="404.webp" />
+    <img
+      class:hidden={$page.status !== 404}
+      draggable="false"
+      class="select-none max-w-lg w-full h-auto"
+      src={Error404}
+      alt="404.webp" />
     <a href="/" class="btn btn-ghost underline text-lg !p4" target="_self">{$LL.Page404BackHome()}</a>
   </article>
 </main>
