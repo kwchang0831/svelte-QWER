41b1ded32220b9d21f3bddef867179d5c3e15ca7
Matthew McLeod <m@qwrk.ai>
2023-08-16 02:32:35 +0200

    ✨ Cloudflare support

--- svelte.config.js
+++ svelte.config.js
@@ -2,25 +2,34 @@ import adapterNode from '@sveltejs/adapter-node';
 import adapterStatic from '@sveltejs/adapter-static';
 import adapterVercel from '@sveltejs/adapter-vercel';
 import adapterNetlify from '@sveltejs/adapter-netlify';
+import adapterCloudflare from '@sveltejs/adapter-cloudflare';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: preprocess({ preserve: ['partytown'] }),
   kit: {
-    adapter: Object.keys(process.env).some((key) => key.includes('VERCEL'))
-      ? adapterVercel()
-      : Object.keys(process.env).some((key) => key.includes('NETLIFY'))
-      ? adapterNetlify()
-      : process.env.ADAPTER === 'node'
+    adapter: getAdapter(),
+    csp: { mode: 'auto' },
+  },
+};
+
+function getAdapter() {
+  if (Object.keys(process.env).some((key) => key.includes('VERCEL'))) {
+    return adapterVercel();
+  } else if (Object.keys(process.env).some((key) => key.includes('NETLIFY'))) {
+    return adapterNetlify();
+  } else if (Object.keys(process.env).some((key) => key.includes('CF_PAGES'))) {
+    return adapterCloudflare();
+  } else {
+    return process.env.ADAPTER === 'node'
       ? adapterNode({ out: 'build' })
       : adapterStatic({
           pages: 'build',
           assets: 'build',
           fallback: null,
-        }),
-    csp: { mode: 'auto' },
-  },
-};
+        });
+  }
+}
 
 export default config;
